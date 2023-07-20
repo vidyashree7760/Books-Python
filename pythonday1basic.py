81ac156e8fc50a383b6dae08085bbd713ed6a8d9
vidyashree7760 <101351444+vidyashree7760@users.noreply.github.com>
2023-07-20 19:18:42 +0530

    Add files via upload

---
+++ pythonday1basic.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello world")
+
+
+# In[2]:
+
+
+Print("hello world")
+
+
+# In[3]:
+
+
+print(hello world)
+
+
+# In[ ]:
+
+
+
+
+
+# In[4]:
+
+
+print(10)
+
+
+# In[5]:
+
+
+print(10.7)
+
+
+# 
+
+# # print hello world
+
+# In[9]:
+
+
+### 1. variables name should start with A-Z,a-z or
+firstname="vidya"
+firstname
+
+
+# In[10]:
+
+
+### 2. variables name should not havr space
+frist name="vidya"
+
+
+# In[12]:
+
+
+name="data"
+name
+
+
+# In[13]:
+
+
+_name="data"
+_name
+
+
+# In[14]:
+
+
++name="data"
++name
+
+
+# In[15]:
+
+
+### 3.Variablename should not start with number
+1name="vidya"
+1name
+
+
+# In[16]:
+
+
+name1="vidya"
+name1
+
+
+# In[17]:
+
+
+### 4. only allowed spl char is_
+first+name="artificial"
+first+name
+
+
+# In[20]:
+
+
+first_name="artificial"
+first_name
+
+
+# data type
+
+# In[21]:
+
+
+x=10.5
+type(x)
+
+
+# In[23]:
+
+
+y=(10)
+type(y)
+
+
+# In[25]:
+
+
+z=true
+type(z)
+
+
+# In[26]:
+
+
+z="true"
+type(z)
+
+
+# In[27]:
+
+
+z=true
+type(z)
+
+
+# In[29]:
+
+
+x=10
+type(x)
+x
+
+
+# In[30]:
+
+
+x,type(x)
+
+
+# In[33]:
+
+
+x,y=10,20
+y
+x
+
+
+# In[34]:
+
+
+x,y
+
+
+# In[35]:
+
+
+y,x
+
+
+# # task
+
+# In[36]:
+
+
+print("vidya")
+
+
+# In[37]:
+
+
+v="vidya"
+type(v)
+
+
+# In[38]:
+
+
+print(25)
+
+
+# In[39]:
+
+
+v=25
+type(v)
+
+
+# In[ ]:
+
+
+
+
