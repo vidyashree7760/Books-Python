2dad300b0836ef41d12c8e660d67f8e626813751
vidyashree7760 <101351444+vidyashree7760@users.noreply.github.com>
2023-07-20 19:20:19 +0530

    Add files via upload

---
+++ data str day2.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### data structures
+# list-> elements are seperated by comma and enclosed with squre braket
+# heterogeneous
+# list can be retirved using index
+# mutable- can make the change after creating the list
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[5]:
+
+
+sample_list=[1,13.6,"hi",12,1]
+sample_list
+
+
+# In[3]:
+
+
+type(sample_list)
+
+
+# In[4]:
+
+
+sample_list[1]
+
+
+# In[8]:
+
+
+sample-list[5]
+
+
+# In[9]:
+
+
+sample_list[-2]
+
+
+# In[10]:
+
+
+sample_list[0]=10
+
+
+# In[13]:
+
+
+sample_list
+
+
+# ###tuple
+
+# ####tuple
+# heterogenous
+# Paranthesis
+# Can retieve data usinng index
+# Immutable
+
+# In[15]:
+
+
+sample_tuple=(1,13.6,"hi",12,1)
+sample_tuple
+
+
+# In[16]:
+
+
+type(sample_tuple)
+
+
+# In[17]:
+
+
+sample_tuple[1]
+
+
+# In[18]:
+
+
+sample_tuple[-2]
+
+
+# In[19]:
+
+
+sample_tuple[10]
+
+
+# In[20]:
+
+
+sample_tuple[0]=10
+
+
+# ## set
+# set is mutable
+# doesnot allow duplicates
+# oredered-fristplace
+# in sett,elemets cannot be retrived using index
+# 
+
+# In[22]:
+
+
+sample_set={1,13.6,"hi",12,1,127,25,25,"apple","king","ball"}
+sample_set
+
+
+# 
+
+# In[23]:
+
+
+sample_set[0]
+
+
+# In[24]:
+
+
+sample_set[o]="hello"
+
+
+# In[28]:
+
+
+sample_set.add(10000)
+
+
+# In[29]:
+
+
+sample_set.remove("hi")
+
+
+# In[30]:
+
+
+sample_set
+
+
+# In[2]:
+
+
+###dictionary
+
+
+# In[4]:
+
+
+sample_dict={1:"apple",2:35.4,3:"data",2:"every",6:35.4,7:35.4,2:"hi"}
+sample_dict
+
+
+# In[5]:
+
+
+sample_dict[3]
+
+
+# In[6]:
+
+
+sample_dict["apple"]
+
+
+# In[8]:
+
+
+sample_dict[7]=100
+
+
+# 
+
+# In[9]:
+
+
+sample_dict
+
+
+# In[11]:
+
+
+sample_dict[10]="business"
+
+
+# In[12]:
+
+
+sample_dict
+
+
+# In[ ]:
+
+
+
+
